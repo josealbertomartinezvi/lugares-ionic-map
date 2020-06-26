c1fe5a098b451a69b6948de705a4bed00a005afa
Jose Martinez <josealbertomartinez.vi@gmail.com>
2020-06-25 23:57:54 -0500

    obtencion de las ubicaciones en el mapa y listarlos

--- src/app/pages/buscar-lugares/buscar-lugares.page.ts
+++ src/app/pages/buscar-lugares/buscar-lugares.page.ts
@@ -1,6 +1,8 @@
+import { async } from '@angular/core/testing';
 import { MapService } from './../../services/map/map.service';
 import { Component, OnInit } from '@angular/core';
 declare let google;
+
 @Component({
   selector: 'app-buscar-lugares',
   templateUrl: './buscar-lugares.page.html',
@@ -10,6 +12,9 @@ export class BuscarLugaresPage implements OnInit {
 
   public lat: number;
   public lng: number;
+  public ubics: any = [];
+  public markers: any = [];
+  i = 0;
 
   constructor(private mapService: MapService) { }
 
@@ -22,10 +27,74 @@ export class BuscarLugaresPage implements OnInit {
     });
   }
 
-  buscarLugar(event){
-    // console.log(event.detail.value)
-    this.mapService.searchPlaces(event.detail.value);
+  sortFloat(a,b) { return a.distancia - b.distancia }
 
+  buscarLugar =  (event) =>{
+    for(var i=0; i<this.markers.length; i++){
+      this.markers[i].setMap(null);
+    }
+    this.ubics = [];
+
+    if(event.detail.value != ''){
+      let data = {
+        location: new google.maps.LatLng(this.lat, this.lng),
+        radius: 1000,
+        keyword: event.detail.value
+      };
+      let service = this.mapService.service;
+      let map = this.mapService.ubicData.map;
+
+      service.nearbySearch(data, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          let infowindow = new google.maps.InfoWindow({});
+          results.map(item => {
+            let marker = new google.maps.Marker({
+              map,
+              position: item.geometry.location,
+            });
+            marker.addListener('click', function() {
+              infowindow.setContent(item.name);
+              infowindow.open(map, this);
+            });
+            this.markers.push(marker);
+            this.calcularDistancia(item);
+          });
+          console.log(this.ubics)
+        }
+      });
+    }
+
+
+  }
+
+  calcularDistancia(item){
+    let request = {
+      origin: this.mapService.ubicData.myUbic,
+      destination: item.geometry.location,
+      travelMode: google.maps.TravelMode.DRIVING,
+      // unitSystem: google.maps.UnitSystem.METRIC
+    }
+    let directionsService = new google.maps.DirectionsService();
+    let data = {};
+    
+    // Routing .slice(0, -3)
+    directionsService.route(request, (result, status) => {
+        if(status == google.maps.DirectionsStatus.OK) {
+          data['nombre'] = item.name;
+          data['direccion'] = result.routes[0].legs[0].end_address;
+          data['distancia'] = result.routes[0].legs[0].distance.text;
+          data['lat_origen'] = request.origin['lat']();
+          data['lng_origen'] = request.origin['lng']();
+          data['lat_destino'] = request.destination['lat']();
+          data['lng_destino'] = request.destination['lng']();
+
+          this.ubics.push(data);
+        }
+    });
+  }
+
+  savePlace(item){
+    console.log(item)
   }
 
 }
--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -7,11 +7,17 @@ declare let google;
 })
 export class MapService {
 
-  map: any;
-  marker: any;
-  myUbic: any;
+  ubicData = {
+    map: '',
+    marker: '',
+    myUbic: ''
+  }
 
-  constructor(public geolocation: Geolocation) { }
+  service: any;
+
+  constructor(public geolocation: Geolocation) { 
+
+  }
 
   /**
    * Obtiene mi ubicación actual
@@ -29,11 +35,12 @@ export class MapService {
    * Renderiza un mapa en pantalla
    */
   async initMap(lat: number, lng: number, id: string){
-    this.myUbic = new google.maps.LatLng(lat, lng);
-    this.map = await new google.maps.Map(document.getElementById(id), {
-      center: this.myUbic,
+    this.ubicData.myUbic = new google.maps.LatLng(lat, lng);
+    this.ubicData.map = await new google.maps.Map(document.getElementById(id), {
+      center: this.ubicData.myUbic,
       zoom: 12
     });
+    this.service = await new google.maps.places.PlacesService(this.ubicData.map)
     // const marker = new google.maps.Marker({
     //   position: {lat, lng},
     //   map: this.map,
@@ -41,52 +48,4 @@ export class MapService {
     // });
   }
 
-  async searchPlaces(palabra){
-    var map = this.map;
-    var marker;
-    var origin = this.myUbic;
-    var request = {
-      location: origin,
-      radius: 5000,
-      keyword: palabra
-    };
-
-    var service = await new google.maps.places.PlacesService(this.map);
-    var directionsService = new google.maps.DirectionsService();
-
-    service.nearbySearch(request, function(results, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        results.map(item => {
-          marker = new google.maps.Marker({
-            map,
-            position: item.geometry.location,
-          });
-          var infowindow = new google.maps.InfoWindow({});
-          marker.addListener('click', function() {
-            console.log(item.name)
-            infowindow.setContent(item.name);
-            infowindow.open(map, this);
-          });
-  
-          //create request
-          var request = {
-              origin,
-              destination: item.geometry.location,
-              travelMode: google.maps.TravelMode.DRIVING,
-              unitSystem: google.maps.UnitSystem.METRIC
-          }
-      
-          // Routing
-          directionsService.route(request, function (result, status) {
-              if(status == google.maps.DirectionsStatus.OK) {
-                item.distancia = result.routes[0].legs[0].distance.text;
-              }
-          });
-
-        });
-
-      }
-      console.log(results)
-    });
-  }
 }
